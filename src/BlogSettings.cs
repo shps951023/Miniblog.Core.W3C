18c7f0f759ab8cc754ccacee12bcc5c85ee90aa7
ITWeiHan <shps951002@gmail.com>
2018-10-01 16:47:09 +0800

    增加是否顯示登入前端介面設定

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -5,5 +5,6 @@
         public string Owner { get; set; } = "The Owner";
         public int PostsPerPage { get; set; } = 2;
         public int CommentsCloseAfterDays { get; set; } = 10;
+        public bool ShowLogin { get; set; } = true;
     }
 }
