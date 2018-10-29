f4ebe9eb702aee2910918c2775034091e2606496
ITWeiHan <shps951002@gmail.com>
2018-10-29 12:02:32 +0800

    SemiNumericComparer 換 PostTitleComparer

--- src/Common/PostTitleComparer.cs
+++ src/Common/PostTitleComparer.cs
@@ -6,30 +6,27 @@ using System.Threading.Tasks;
 
 namespace Miniblog.Core
 {
-    public class SemiNumericComparer : IComparer<string>
+    //邏輯:
+    //	1.實作 `IComparer` 介面
+    public class PostTitleComparer : IComparer<string>
     {
-
-        static int ExtractNumber(string text)
+        //	2.使用正則抓出`()`裡面的數字值
+        static int UseRegexGetTitleNumber(string text)
         {
             Match match = Regex.Match(text, @"(\d+)");
             if (match == null)
-            {
                 return 0;
-            }
-
             int value;
             if (!int.TryParse(match.Value, out value))
-            {
                 return 0;
-            }
-
             return value;
         }
 
+        //	3.實作` Compare 比較方法`，如果前者大於後者返回1整數，如果前者等於後者返回0整數，如果前者小於後者返回 - 1。藉此來比較大小作排序。
         public int Compare(string s1, string s2)
         {
-            var s1Val = ExtractNumber(s1);
-            var s2Val = ExtractNumber(s2);
+            var s1Val = UseRegexGetTitleNumber(s1);
+            var s2Val = UseRegexGetTitleNumber(s2);
             if (s1Val > s2Val) return 1;
             if (s1Val < s2Val) return -1;
             if (s1Val == s2Val) return 0;
